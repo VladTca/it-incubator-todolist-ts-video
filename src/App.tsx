d5a994d1e74615cb150651f3e1fc7090539a4541
VladTca <votsats@gmail.com>
2024-03-18 22:19:04 +0100

    Added new files and configurations for project setup. Updated index and App components with task lists and checkboxes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
 import React from "react";
-
 import "./App.css";
+import { TaskType, Todolist } from "./Todolist";
 
 function App() {
+  let tasks1: Array<TaskType> = [
+    { id: 1, title: "CSS", isDone: true },
+    { id: 2, title: "JS", isDone: true },
+    { id: 3, title: "REACT", isDone: false },
+  ];
+  let tasks2: Array<TaskType> = [
+    { id: 1, title: "Terminator", isDone: true },
+    { id: 2, title: "XXX", isDone: false },
+    { id: 3, title: "JETofFort", isDone: true },
+  ];
   return (
     <div className="App">
-      <Todolist />
+      <Todolist title="What to learn" tasks={tasks1} />
+      <Todolist title="Movies" tasks={tasks2} />
+      {/*<Todolist title="Songs" />*/}
+
+      {/*<input type="checkbox" />*/}
+      {/*<input type="date" />*/}
+      {/*<input placeholder="it-incubator" />*/}
     </div>
   );
 }
 
 export default App;
-
-function Todolist() {
-  return <div>TODOLIST</div>;
-}
