f80940c26888cb96f4a4fbefb70ff9989db8784c
VladTca <votsats@gmail.com>
2024-06-23 02:28:01 +0200

    🎨 Improve structure / format of the code.

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,6 +1,7 @@
 import { FilterValuesType, TaskType } from "./App";
 import { ChangeEvent, KeyboardEvent, useState } from "react";
 import { Button } from "./Button";
+import { AddItemForm } from "./AddItemForm";
 
 type PropsType = {
   title: string;
@@ -31,29 +32,6 @@ export const Todolist = (props: PropsType) => {
     todolistId,
   } = props;
 
-  const [taskTitle, setTaskTitle] = useState("");
-  const [error, setError] = useState<string | null>(null);
-
-  const addTaskHandler = () => {
-    if (taskTitle.trim() !== "") {
-      addTask(todolistId, taskTitle.trim());
-      setTaskTitle("");
-    } else {
-      setError("Title is required");
-    }
-  };
-
-  const changeTaskTitleHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    setTaskTitle(event.currentTarget.value);
-  };
-
-  const addTaskOnKeyUpHandler = (event: KeyboardEvent<HTMLInputElement>) => {
-    setError(null);
-    if (event.key === "Enter") {
-      addTaskHandler();
-    }
-  };
-
   const changeFilterTasksHandler = (filter: FilterValuesType) => {
     changeFilter(todolistId, filter);
   };
@@ -64,16 +42,7 @@ export const Todolist = (props: PropsType) => {
         {title}
         <button onClick={() => removeTodolist(props.todolistId)}>x</button>
       </h3>
-      <div>
-        <input
-          className={error ? "error" : ""}
-          value={taskTitle}
-          onChange={changeTaskTitleHandler}
-          onKeyUp={addTaskOnKeyUpHandler}
-        />
-        <Button title={"+"} onClick={addTaskHandler} />
-        {error && <div className={"error-message"}>{error}</div>}
-      </div>
+      <AddItemForm addTask={addTask} todolistId={todolistId} />
       {tasks.length === 0 ? (
         <p>Тасок нет</p>
       ) : (
